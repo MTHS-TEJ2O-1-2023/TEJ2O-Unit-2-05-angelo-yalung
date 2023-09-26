767b19856508f5d8b957424693a10b26acf8d27b
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-26 13:25:40 -0400

    f

--- main.ts
+++ main.ts
@@ -9,6 +9,9 @@ basic.clearScreen()
 basic.pause(1000)
 
 input.onButtonPressed(Button.A, function () {
+    basic.showString('The temperature is')
+    basic.showNumber(input.temperature())
+    basic.showString('Degrees Celsius, and the temperature in Kelvin is')
     basic.showNumber(input.temperature() + 273)
     basic.showString('Kelvin')
 })
