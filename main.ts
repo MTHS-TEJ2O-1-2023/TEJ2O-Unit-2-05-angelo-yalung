db32bc124c7314611da5fa0b29d1c091f9c3a8d0
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-26 13:22:04 -0400

    dumbed down 14x

--- main.ts
+++ main.ts
@@ -8,11 +8,7 @@
 basic.clearScreen()
 basic.pause(1000)
 
-let temperature = input.temperature()
-
 input.onButtonPressed(Button.A, function () {
-    temperature = input.temperature()
-    basic.showNumber(input.temperature())
-    basic.showNumber(input.temperature() + 273.15)
+    basic.showNumber(input.temperature() + 273)
     basic.showString('Kelvin')
 })
