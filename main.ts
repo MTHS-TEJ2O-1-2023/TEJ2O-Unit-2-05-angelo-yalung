b0dc6e0be3cae397de5eff15f18d643613bcc3ab
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-26 13:19:50 -0400

    Restore '9/25/2023, 1:02:21 PM KELVIN'

--- main.ts
+++ main.ts
@@ -5,16 +5,14 @@
  * This program shows the current temperature in Kelvin
 */
 
-
 basic.clearScreen()
 basic.pause(1000)
 
-let temperature: number
-
-// Turns the temperature in degrees celsius into kelvin
+let temperature = input.temperature()
 
 input.onButtonPressed(Button.A, function () {
     temperature = input.temperature()
-    basic.showNumber(input.temperature() + 273)
+    basic.showNumber(input.temperature())
+    basic.showNumber(input.temperature() + 273.15)
     basic.showString('Kelvin')
 })
