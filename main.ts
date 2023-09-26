112d1be9f4536d99dee8dc277329cf2118874d0d
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-26 13:07:17 -0400

    ljk

--- main.ts
+++ main.ts
@@ -11,8 +11,10 @@ basic.pause(1000)
 
 let temperature: number
 
-/// Turns the temperature in degrees celsius into kelvin
+// Turns the temperature in degrees celsius into kelvin
+
 input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
     basic.showNumber(input.temperature() + 273)
     basic.showString('Kelvin')
 })
