ab7ed5a87ec7f37eb7d7e2a6587b679229af1011
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-30 09:22:39 -0400

    Restore '9/25/2023, 12:51:00 PM Initial commit'

--- main.ts
+++ main.ts
@@ -1,23 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Angelo Yalung
- * Created on: Sep 2023
- * This program shows the current temperature in Kelvin
+ * Created by: Mr. Coxall
+ * Created on: Sep 2020
+ * This program ...
 */
 
-basic.clearScreen()
-basic.pause(1000)
-
-let temperature: number
-let kelvin: number
-
-input.onButtonPressed(Button.A, function () {
-  // actual caluclation
-  temperature = input.temperature()
-  kelvin = temperature + 273.15
-  kelvin = Math.round(kelvin)
-
-  // text
-  basic.clearScreen()
-  basic.showString('Temperature in Kelvin is:' + kelvin.toString())
-})
+basic.showString('Hello, World!')
