c6ab0a5b7f17335c5bfacf62b9a5b8ba03046f6d
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-30 13:35:58 +0000

    s

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Angelo Yalung
+Created on: Sep 2023
+This module return percentage of light.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        temp = temperature
+        kelvin = temperature + 273
+        display.show(str(kelvin))
