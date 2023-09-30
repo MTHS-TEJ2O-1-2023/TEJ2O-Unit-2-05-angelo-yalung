a4d38a89d890e2257c9aee3599bcbc4b99707924
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-30 08:50:54 -0400

    p-raying

--- main.ts
+++ main.ts
@@ -8,11 +8,12 @@
 basic.clearScreen()
 basic.pause(1000)
 
+input.onButtonPressed(Button.A, function () {
+  basic.showString('The temp is')
+  basic.showNumber(input.temperature())
 let temperature = input.temperature()
 
-input.onButtonPressed(Button.A, function () {
-  basic.showString('The temperature is')
-  basic.showNumber(temperature)
+  // actual caluclation
   basic.showString('Degrees Celsius, and the temperature in Kelvin is')
   basic.showNumber(temperature + 273)
   basic.showString('Kelvin')
